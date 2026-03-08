968ff384b74a13ba8439d4dc926df36a61f46bb6
pedrodevsz <pedrodevs25@gmail.com>
2026-03-08 15:49:50 -0300

    fix: ajusted  dynamic page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { Dashboard } from "@/components/homepage/dashboard";
 
 export default function Home() {
--- src/components/homepage/my-projects/card-container.tsx
+++ src/components/homepage/my-projects/card-container.tsx
@@ -1,5 +1,3 @@
-export const dynamic = "force-dynamic";
-
 import { getProjects } from "@/lib/mongodb/get-projects"
 import { ProjectsGrid } from "./projects-grid"
 
